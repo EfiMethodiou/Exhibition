267332fc8ee95eb6961e7232119b2dfcc3da6b2f
EfiMethodiou <37740639+EfiMethodiou@users.noreply.github.com>
2021-04-14 11:23:49 +0200

    modidied files for slider

--- js/level_3.js
+++ js/level_3.js
@@ -4,71 +4,10 @@ function backToLevel2(){
 }
 
 function openMS_pop(){    
-    document.getElementById("popupBackg").style.display = "block";
+    document.getElementById("infoBackg").style.display = "block";
     document.getElementById("info_popup_M_S").style.display = "block";
 }
 function closeMS_pop(){    
-    document.getElementById("popupBackg").style.display = "none";
+    document.getElementById("infoBackg").style.display = "none";
     document.getElementById("info_popup_M_S").style.display = "none";
-}
-
-//SLIDER FUNCTIONS
-function goLeft(){
-    if(document.getElementById("greenGroup").style.display ==="inline" && 
-    document.getElementById("redGroup").style.display ==="inline"){      
-        document.getElementById("violetGroup2").style.display = "inline";
-        document.getElementById("greenGroup").style.paddingLeft = "0px";
-        document.getElementById("violetGroup2").style.paddingLeft = "10%";
-        document.getElementById("redGroup").style.display = "none";
-    }   
-    else if(document.getElementById("greenGroup").style.display ==="inline" && 
-    document.getElementById("violetGroup2").style.display ==="inline"){   
-        document.getElementById("violetGroup2").style.display = "none"; 
-        document.getElementById("greenGroup").style.paddingLeft = "10%";
-        document.getElementById("violetGroup").style.display = "inline";    
-        document.getElementById("orangeGroup").style.display = "inline";
-        document.getElementById("greenGroup").style.display = "none";
-    }
-    else if(document.getElementById("violetGroup").style.display === "none"){
-        document.getElementById("orangeGroup").style.display = "none";
-        document.getElementById("redGroup").style.display = "inline";
-        document.getElementById("greenGroup").style.float = "none";
-        document.getElementById("greenGroup").style.paddingLeft = "10%";
-        document.getElementById("greenGroup").style.display = "inline";      
-    }   
-    else{
-        document.getElementById("violetGroup").style.display = "none";
-        document.getElementById("redGroup").style.display = "inline";             
-    }
-   
-}
-
-function goRight(){
-    if(document.getElementById("violetGroup2").style.display === "inline" &&
-    document.getElementById("greenGroup").style.display === "inline"){
-        document.getElementById("violetGroup2").style.display = "none";
-        document.getElementById("greenGroup").style.float = "none";
-        document.getElementById("greenGroup").style.paddingLeft = "10%";
-        document.getElementById("redGroup").style.display = "inline";        
-    }
-    else if(document.getElementById("redGroup").style.display === "inline" &&
-    document.getElementById("greenGroup").style.display === "inline"){       
-        document.getElementById("greenGroup").style.display = "none";
-        document.getElementById("orangeGroup").style.display = "inline";
-    }
-    //return to default
-    else if(document.getElementById("orangeGroup").style.display === "inline" && 
-    document.getElementById("redGroup").style.display === "inline"){        
-        document.getElementById("violetGroup").style.display = "inline";
-        document.getElementById("redGroup").style.display = "none";        
-    }
-    else {        
-        document.getElementById("greenGroup").style.display = "inline";
-        document.getElementById("violetGroup").style.display = "none";
-        document.getElementById("violetGroup2").style.display = "inline";
-        document.getElementById("orangeGroup").style.display = "none";
-        document.getElementById("greenGroup").style.paddingLeft = "0px";
-        document.getElementById("violetGroup2").style.paddingLeft = "10%";
-        document.getElementById("redGroup").style.display = "none";
-    }
 }
---
+++ js/slider.js
@@ -0,0 +1,102 @@
+
+//POPUP FUNCTIONS FOR THE GROUPS
+function openViolet(){
+    document.getElementById("violetPopup").style.display ="block";
+    document.getElementById("popupBackg").style.display ="block";
+    document.getElementById("infoBackg").style.display = "none";
+    document.getElementById("info_popup_M_S").style.display = "none";
+}
+
+function openOrange(){
+    document.getElementById("orangePopup").style.display ="block";
+    document.getElementById("popupBackg").style.display ="block";
+    document.getElementById("infoBackg").style.display = "none";
+    document.getElementById("info_popup_M_S").style.display = "none";
+}
+
+function openRed(){
+    document.getElementById("redPopup").style.display ="block";
+    document.getElementById("popupBackg").style.display ="block";
+    document.getElementById("infoBackg").style.display = "none";
+    document.getElementById("info_popup_M_S").style.display = "none";
+}
+
+function openGreen(){
+    document.getElementById("greenPopup").style.display ="block";
+    document.getElementById("popupBackg").style.display ="block";
+    document.getElementById("infoBackg").style.display = "none";
+    document.getElementById("info_popup_M_S").style.display = "none";
+}
+function closePopup(){
+    document.getElementById("violetPopup").style.display ="none";
+    document.getElementById("orangePopup").style.display ="none";
+    document.getElementById("redPopup").style.display ="none";
+    document.getElementById("greenPopup").style.display ="none";
+    document.getElementById("popupBackg").style.display ="none";
+}
+
+
+
+
+/*
+    SLIDER FUNCTIONS
+*/
+function goLeft(){
+    if(document.getElementById("greenGroup").style.display ==="inline" && 
+    document.getElementById("redGroup").style.display ==="inline"){      
+        document.getElementById("violetGroup2").style.display = "inline";
+        document.getElementById("greenGroup").style.paddingLeft = "0px";
+        document.getElementById("violetGroup2").style.paddingLeft = "10%";
+        document.getElementById("redGroup").style.display = "none";
+    }   
+    else if(document.getElementById("greenGroup").style.display ==="inline" && 
+    document.getElementById("violetGroup2").style.display ==="inline"){   
+        document.getElementById("violetGroup2").style.display = "none"; 
+        document.getElementById("greenGroup").style.paddingLeft = "10%";
+        document.getElementById("violetGroup").style.display = "inline";    
+        document.getElementById("orangeGroup").style.display = "inline";
+        document.getElementById("greenGroup").style.display = "none";
+    }
+    else if(document.getElementById("violetGroup").style.display === "none"){
+        document.getElementById("orangeGroup").style.display = "none";
+        document.getElementById("redGroup").style.display = "inline";
+        document.getElementById("greenGroup").style.float = "none";
+        document.getElementById("greenGroup").style.paddingLeft = "10%";
+        document.getElementById("greenGroup").style.display = "inline";      
+    }   
+    else{
+        document.getElementById("violetGroup").style.display = "none";
+        document.getElementById("redGroup").style.display = "inline";             
+    }
+   
+}
+
+function goRight(){
+    if(document.getElementById("violetGroup2").style.display === "inline" &&
+    document.getElementById("greenGroup").style.display === "inline"){
+        document.getElementById("violetGroup2").style.display = "none";
+        document.getElementById("greenGroup").style.float = "none";
+        document.getElementById("greenGroup").style.paddingLeft = "10%";
+        document.getElementById("redGroup").style.display = "inline";        
+    }
+    else if(document.getElementById("redGroup").style.display === "inline" &&
+    document.getElementById("greenGroup").style.display === "inline"){       
+        document.getElementById("greenGroup").style.display = "none";
+        document.getElementById("orangeGroup").style.display = "inline";
+    }
+    //return to default
+    else if(document.getElementById("orangeGroup").style.display === "inline" && 
+    document.getElementById("redGroup").style.display === "inline"){        
+        document.getElementById("violetGroup").style.display = "inline";
+        document.getElementById("redGroup").style.display = "none";        
+    }
+    else {        
+        document.getElementById("greenGroup").style.display = "inline";
+        document.getElementById("violetGroup").style.display = "none";
+        document.getElementById("violetGroup2").style.display = "inline";
+        document.getElementById("orangeGroup").style.display = "none";
+        document.getElementById("greenGroup").style.paddingLeft = "0px";
+        document.getElementById("violetGroup2").style.paddingLeft = "10%";
+        document.getElementById("redGroup").style.display = "none";
+    }
+}
