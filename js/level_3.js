4ae8c6f454467111ff09f49ee93b360f42ac5637
EfiMethodiou <37740639+EfiMethodiou@users.noreply.github.com>
2021-04-23 15:14:49 +0200

    animation added

--- js/level_3.js
+++ js/level_3.js
@@ -1,24 +1,28 @@
 /*Go back to level_2 page*/
-function backToLevel2(){
-    window.location.href = "../level_2.html";
+function backToLevel2() {
+  window.location.href = "../level_2.html";
 }
 
-function openMS_pop(){    
-    document.getElementById("infoBackg").style.display = "block";
-    document.getElementById("info_popup").style.display = "block";
+function openMS_pop() {
+  document.getElementById("infoBackg").style.display = "block";
+  document.getElementById("info_popup").style.display = "block";
 }
-function closeMS_pop(){    
-    document.getElementById("infoBackg").style.display = "none";
-    document.getElementById("info_popup").style.display = "none";
+function closeMS_pop() {
+  document.getElementById("infoBackg").style.display = "none";
+  document.getElementById("info_popup").style.display = "none";
 }
 
-
-//Node glowing 
+//Node glowing
 
 //Navigation functions
-function gotoRegnumAnimale(){
-    window.location.href = "larys_lub.html";
+function gotoRegnumAnimale() {
+    setTimeout(function () {
+        window.location.href = "larys_lub.html";
+      }, 2100);
+  // window.location.href = "larys_lub.html";
+  document.getElementsByClassName("ripple-open");
+  $(".ripple-open").toggleClass("ripple-open-active");
 }
-function gotoBrokenLinks(){
-    window.location.href = "maria_subc.html";
+function gotoBrokenLinks() {
+  window.location.href = "maria_subc.html";
 }
