8c9c4ab7d8e27274018d657016799a16aaf8832e
EfiMethodiou <37740639+EfiMethodiou@users.noreply.github.com>
2021-04-27 11:56:44 +0200

    added the loader

--- js/script.js
+++ js/script.js
@@ -7,94 +7,97 @@
 
 */
 
+//the loader will appear only the first time that the user opens the page
+function firstLoad() {
+  if (localStorage.getItem("hasCodeRunBefore") === null) {
+    console.log("it runs");
+    document.getElementById("level_0").style.display = "block";
+    setTimeout(function () {
+      $(".level_0").fadeToggle();
+    }, 4500);
+    localStorage.setItem("hasCodeRunBefore", true);
+  }
+}
+window.onload = firstLoad();
 
-/*timer for level_0 to disappear
-setTimeout(function () {
-    $(".level_0").fadeToggle();
-    window.location.href = "html/main.html";
-}, 4500);*/
 
 /*________________MAIN_______PAGE__________*/
 /*Open popups from the menu*/
-function openAbout(){
-document.getElementById("about").style.display = "block";
-document.getElementById("aboutBackg").style.display = "block";
+function openAbout() {
+  document.getElementById("about").style.display = "block";
+  document.getElementById("aboutBackg").style.display = "block";
 }
-function openRhizonet(){
-    document.getElementById("rhizonet").style.display = "block";
-    document.getElementById("rhizBackg").style.display = "block";
+function openRhizonet() {
+  document.getElementById("rhizonet").style.display = "block";
+  document.getElementById("rhizBackg").style.display = "block";
 }
 /*Close about asn*/
-function closeAbout(){
-    document.getElementById("about").style.display = "none";
-    document.getElementById("aboutBackg").style.display = "none";
+function closeAbout() {
+  document.getElementById("about").style.display = "none";
+  document.getElementById("aboutBackg").style.display = "none";
 }
-function closeRhizo(){
-    document.getElementById("rhizonet").style.display ="none";
-    document.getElementById("rhizBackg").style.display = "none";
+function closeRhizo() {
+  document.getElementById("rhizonet").style.display = "none";
+  document.getElementById("rhizBackg").style.display = "none";
 }
 
 /*Close menu when category is clicked*/
-function closeMenu() {    
-    document.getElementById("menuCheck").checked = false;  
-    document.getElementById("menuBackg").style.display = "none";  
+function closeMenu() {
+  document.getElementById("menuCheck").checked = false;
+  document.getElementById("menuBackg").style.display = "none";
 }
-function openMenuBack(){
-    document.getElementById("menuBackg").style.display = "block"; 
+function openMenuBack() {
+  document.getElementById("menuBackg").style.display = "block";
 }
 
-function openPopup(){
-    document.getElementById("popup_capture").style.display ="block";
-    document.getElementById("popup_captureBackg").style.display ="block";
+function openPopup() {
+  document.getElementById("popup_capture").style.display = "block";
+  document.getElementById("popup_captureBackg").style.display = "block";
 }
-function closePopCapt(){
-    document.getElementById("popup_capture").style.display ="none";
-    document.getElementById("popup_captureBackg").style.display ="none";
+function closePopCapt() {
+  document.getElementById("popup_capture").style.display = "none";
+  document.getElementById("popup_captureBackg").style.display = "none";
 }
 /*Go to level2 page*/
-function gotoLevel2(){
-    window.location.href = "html/level_2.html";
+function gotoLevel2() {
+  window.location.href = "html/level_2.html";
 }
-
-
-
 
 /*________________LEVEL2_______PAGE____________*/
 /*Go back to main page*/
-function backToMain(){
-    window.location.href = "../index.html";
+function backToMain() {
+  window.location.href = "../index.html";
 }
 
 /*
 Open ABOUT THE EXHIBITION popup*/
-function openAbExhib(){
-    document.getElementById("about__exhibition").style.display = "block";
-    document.getElementById("menuPopupBackg").style.display = "block";
+function openAbExhib() {
+  document.getElementById("about__exhibition").style.display = "block";
+  document.getElementById("menuPopupBackg").style.display = "block";
 }
 /*
 Open AUGMENTED REALITY popup*/
-function openArPop(){
-    document.getElementById("augmented_reality").style.display = "block";
-    document.getElementById("menuPopupBackg").style.display = "block";
+function openArPop() {
+  document.getElementById("augmented_reality").style.display = "block";
+  document.getElementById("menuPopupBackg").style.display = "block";
 }
 /*
 Open RESEARCH popup*/
-function openResearPop(){
-    document.getElementById("research").style.display = "block";
-    document.getElementById("menuPopupBackg").style.display = "block";
+function openResearPop() {
+  document.getElementById("research").style.display = "block";
+  document.getElementById("menuPopupBackg").style.display = "block";
 }
 
 /*Close popup Menus*/
-function closePopupMenu(){
-    document.getElementById("about__exhibition").style.display = "none";
-    document.getElementById("augmented_reality").style.display = "none"
-    document.getElementById("research").style.display = "none";;
-    document.getElementById("menuPopupBackg").style.display = "none";
-} 
-
+function closePopupMenu() {
+  document.getElementById("about__exhibition").style.display = "none";
+  document.getElementById("augmented_reality").style.display = "none";
+  document.getElementById("research").style.display = "none";
+  document.getElementById("menuPopupBackg").style.display = "none";
+}
 
 /*
 Go to level2 page*/
-function gotoMariaSubc(){
-    window.location.href = "artist/maria_subc.html";
+function gotoMariaSubc() {
+  window.location.href = "artist/maria_subc.html";
 }
