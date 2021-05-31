acd228214a68caa1745a561c3ce30c8824848ca7
EfiMethodiou <37740639+EfiMethodiou@users.noreply.github.com>
2021-05-31 14:36:55 +0200

    changed the description animation

--- js/script.js
+++ js/script.js
@@ -55,7 +55,6 @@ function closeMenu() {
 function openMenuBack() {
   document.getElementById("menuBackg").style.display = "block";
 }
-
 function openPopup() {
   document.getElementById("popup_capture").style.display = "block";
   document.getElementById("popup_captureBackg").style.display = "block";
@@ -64,6 +63,64 @@ function closePopCapt() {
   document.getElementById("popup_capture").style.display = "none";
   document.getElementById("popup_captureBackg").style.display = "none";
 }
+
+/*Description animation loops*/
+
+if(document.title === "ASN RHIZO-NETWORK"){
+  var i = 0; 
+
+  function myLoop1() {
+    setTimeout(function () {
+      i++; 
+      if (i < 10) {
+        myLoop1(); 
+        animation1Loop();
+      } 
+    }, 20000);
+  }
+  myLoop1();
+  
+  var opdesc1 = ["0.8s", "20s","40s","60s", "80s", "100s", "120s", "140s","160s","180s"];
+  var opdesc2 = ["5.8s", "25s", "45s","65s", "85s", "105s","125s", "145s","165s", "185s"];
+  var opdesc3 = ["10.8s","30s","50s","70s", "90s", "110s", "130s", "150s", "170s", "195s"];
+  function animation1Loop() {
+    document.getElementById("desc1").style.animationDelay = opdesc1[i];
+    document.getElementById("desc2").style.animationDelay = opdesc2[i];
+    document.getElementById("desc3").style.animationDelay = opdesc3[i];
+    console.log(i);
+  }
+  animation1Loop();
+}
+
+/*animation loop for second description*/
+if(document.title === "OUR BIO-TECH PLANET"){
+  var x = 0; //  set your counter to 1
+
+  function myLoop2() {
+    setTimeout(function () {
+      x++; 
+      if (x < 10) {
+        myLoop2(); 
+        animation2Loop();
+      } 
+    }, 15000);
+  }
+  myLoop2();
+  
+  var opdesc2_1 = ["0.8s", "15s","30s","45s", "60s", "75s", "90s", "105s","120s","135s"];
+  var opdesc2_2 = ["5.8s", "20s", "35s","50s", "65s", "80s","95s", "110s","125s", "140s"];
+  
+  function animation2Loop() {
+    document.getElementById("desc2_1").style.animationDelay = opdesc2_1[x];
+    document.getElementById("desc2_2").style.animationDelay = opdesc2_2[x];
+    
+    console.log(x);
+  }
+  animation2Loop();
+}
+
+
+
 /*Go to level2 page*/
 function gotoLevel2() {
   window.location.href = "exhibition/our_bio_tech_planet.html";
